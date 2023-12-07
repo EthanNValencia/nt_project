b32bebd4647ba43cff1412e0c99416239428800b
EthanNValencia <enephew2@mail.valenciacollege.edu>
2023-12-07 17:00:28 -0500

    Added install script.

---
+++ install.sh
@@ -0,0 +1,8 @@
+cd naming_server/ && mvn clean install
+cd ../common/ && mvn clean install
+cd ../api_gateway/ && mvn clean install
+cd ../email_service/ && mvn clean install
+cd ../security_service/ && mvn clean install
+cd ../sms_service/ && mvn clean install
+cd ../error_service/ && mvn clean install
+cd ../npt/ && mvn clean install
